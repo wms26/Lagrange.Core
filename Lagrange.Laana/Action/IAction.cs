cf530060b6f24954273f3b0ed4c8cdf763f1aca6
Wesley F. Young <willamwei@outlook.com>
2024-12-11 20:27:16 +0800

    [Laana] SendMessage Action

---
+++ Lagrange.Laana/Action/IAction.cs
@@ -0,0 +1,15 @@
+﻿using Laana;
+using Lagrange.Core;
+
+namespace Lagrange.Laana.Action;
+
+public interface IAction<in TActionPing, TActionPong>
+{
+    public Task<TActionPong> Handle(BotContext bot, TActionPing ping);
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ActionHandlerOf(LaanaActionPing.PingOneofCase action) : Attribute
+{
+    public LaanaActionPing.PingOneofCase Action { get; } = action;
+}
---
+++ Lagrange.Laana/Action/Message/SendMessageAction.cs
@@ -0,0 +1,25 @@
+﻿using Laana;
+using Lagrange.Core;
+using Lagrange.Core.Common.Interface.Api;
+using Lagrange.Laana.Common;
+
+namespace Lagrange.Laana.Action.Message;
+
+[ActionHandlerOf(LaanaActionPing.PingOneofCase.SendMessage)]
+public class SendMessageAction : IAction<SendMessagePing, SendMessagePong>
+{
+    public async Task<SendMessagePong> Handle(BotContext bot, SendMessagePing ping)
+    {
+        var messageResult = await bot.SendMessage(ping.Message.ToMessageChain(ping.TargetPeer));
+        if (messageResult.Result != 0)
+        {
+            throw new Exception($"Failed to send message. Error code: {messageResult.Result}");
+        }
+        if (messageResult.Sequence is null or 0)
+        {
+            throw new Exception("Failed to send message. No sequence returned. Error code: 9000");
+        }
+        // TODO: Save this message to database
+        return new SendMessagePong { MsgSeq = (ulong)messageResult.Sequence! };
+    }
+}
