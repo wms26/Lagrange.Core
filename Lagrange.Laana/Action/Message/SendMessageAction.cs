a864d099a6991ed3255ae15741f13c5357aa4850
Wesley F. Young <willamwei@outlook.com>
2024-12-12 10:40:52 +0800

    [Laana] Move store & restore logic into class MessageRecord

--- Lagrange.Laana/Action/Message/SendMessageAction.cs
+++ Lagrange.Laana/Action/Message/SendMessageAction.cs
@@ -25,7 +25,7 @@ namespace Lagrange.Laana.Action.Message
                 throw new Exception("Failed to send message. No sequence returned. Error code: 9000");
             }
 
-            var messageRecord = new MessageRecord
+            var messageRecord = new MessageRecord(ping.Message)
             {
                 MsgSeq = (ulong)messageResult.Sequence,
                 Time = messageResult.Timestamp * 1000,
@@ -37,69 +37,6 @@ namespace Lagrange.Laana.Action.Message
                 },
                 PeerUin = ping.TargetPeer.Uin
             };
-
-            switch (ping.Message.ContentCase)
-            {
-                case LaanaOutgoingMessage.ContentOneofCase.Bubble:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.Bubble;
-                    messageRecord.Content = ping.Message.Bubble.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.File:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.File;
-                    messageRecord.Content = ping.Message.File.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.SingleImage:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.SingleImage;
-                    messageRecord.Content = ping.Message.SingleImage.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.MarketFace:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.MarketFace;
-                    messageRecord.Content = ping.Message.MarketFace.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.Video:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.Video;
-                    messageRecord.Content = ping.Message.Video.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.Voice:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.Voice;
-                    messageRecord.Content = ping.Message.Voice.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.LinkCard:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.LinkCard;
-                    messageRecord.Content = ping.Message.LinkCard.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.ContactCard:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.ContactCard;
-                    messageRecord.Content = ping.Message.ContactCard.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.MusicCard:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.MusicCard;
-                    messageRecord.Content = ping.Message.MusicCard.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.LocationCard:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.LocationCard;
-                    messageRecord.Content = ping.Message.LocationCard.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.ForwardedMessage:
-                    // Because it is constructed (fake), there is no reason to store it as-is.
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.ExtendedMessage;
-                    messageRecord.Content = new LaanaMessage.Types.ExtendedMessage
-                    {
-                        Type = "ConstructedForwardedMessage", 
-                        Content = ByteString.CopyFrom(ping.Message.ForwardedMessage.Serialize())
-                    }.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.XmlMessage:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.XmlMessage;
-                    messageRecord.Content = ping.Message.XmlMessage.Serialize().ToArray();
-                    break;
-                case LaanaOutgoingMessage.ContentOneofCase.ArkMessage:
-                    messageRecord.ContentCase = LaanaMessage.ContentOneofCase.ArkMessage;
-                    messageRecord.Content = ping.Message.ArkMessage.Serialize().ToArray();
-                    break;
-                default:
-                    throw new Exception("Unexpected message content type.");
-            }
 
             db.GetCollection<MessageRecord>().Insert(messageRecord);
             
--- Lagrange.Laana/Common/MessageRecord.cs
+++ Lagrange.Laana/Common/MessageRecord.cs
@@ -1,4 +1,6 @@
-﻿using Laana;
+﻿using Google.Protobuf;
+using Laana;
+using Lagrange.Core.Utility.Extension;
 
 namespace Lagrange.Laana.Common
 {
@@ -24,68 +26,131 @@ namespace Lagrange.Laana.Common
         public LaanaMessage.ContentOneofCase ContentCase { get; set; }
         
         public byte[] Content { get; set; }
-    }
-    
-    public static class MessageRecordEx
-    {
-        public static LaanaMessage Restore(this MessageRecord record)
+        
+        public MessageRecord(LaanaOutgoingMessage laanaOutgoingMessage)
+        {
+            switch (laanaOutgoingMessage.ContentCase)
+            {
+                case LaanaOutgoingMessage.ContentOneofCase.Bubble:
+                    ContentCase = LaanaMessage.ContentOneofCase.Bubble;
+                    Content = laanaOutgoingMessage.Bubble.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.File:
+                    ContentCase = LaanaMessage.ContentOneofCase.File;
+                    Content = laanaOutgoingMessage.File.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.SingleImage:
+                    ContentCase = LaanaMessage.ContentOneofCase.SingleImage;
+                    Content = laanaOutgoingMessage.SingleImage.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.MarketFace:
+                    ContentCase = LaanaMessage.ContentOneofCase.MarketFace;
+                    Content = laanaOutgoingMessage.MarketFace.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.Video:
+                    ContentCase = LaanaMessage.ContentOneofCase.Video;
+                    Content = laanaOutgoingMessage.Video.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.Voice:
+                    ContentCase = LaanaMessage.ContentOneofCase.Voice;
+                    Content = laanaOutgoingMessage.Voice.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.LinkCard:
+                    ContentCase = LaanaMessage.ContentOneofCase.LinkCard;
+                    Content = laanaOutgoingMessage.LinkCard.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.ContactCard:
+                    ContentCase = LaanaMessage.ContentOneofCase.ContactCard;
+                    Content = laanaOutgoingMessage.ContactCard.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.MusicCard:
+                    ContentCase = LaanaMessage.ContentOneofCase.MusicCard;
+                    Content = laanaOutgoingMessage.MusicCard.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.LocationCard:
+                    ContentCase = LaanaMessage.ContentOneofCase.LocationCard;
+                    Content = laanaOutgoingMessage.LocationCard.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.ForwardedMessage:
+                    // Because it is constructed (fake), there is no reason to store it as-is.
+                    ContentCase = LaanaMessage.ContentOneofCase.ExtendedMessage;
+                    Content = new LaanaMessage.Types.ExtendedMessage
+                    {
+                        Type = "ConstructedForwardedMessage", 
+                        Content = ByteString.CopyFrom(laanaOutgoingMessage.ForwardedMessage.Serialize())
+                    }.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.XmlMessage:
+                    ContentCase = LaanaMessage.ContentOneofCase.XmlMessage;
+                    Content = laanaOutgoingMessage.XmlMessage.Serialize().ToArray();
+                    break;
+                case LaanaOutgoingMessage.ContentOneofCase.ArkMessage:
+                    ContentCase = LaanaMessage.ContentOneofCase.ArkMessage;
+                    Content = laanaOutgoingMessage.ArkMessage.Serialize().ToArray();
+                    break;
+                default:
+                    throw new Exception("Unexpected message content type.");
+            }
+        }
+
+        public LaanaMessage Restore()
         {
             var message = new LaanaMessage
             {
-                MsgSeq = record.MsgSeq,
-                Time = record.Time,
-                SenderUin = record.SenderUin,
+                MsgSeq = MsgSeq,
+                Time = Time,
+                SenderUin = SenderUin,
                 Peer = new LaanaPeer
                 {
-                    Type = record.Type switch
+                    Type = Type switch
                     {
-                        MessageRecord.ChatType.Group => LaanaPeer.Types.Type.Group,
+                        ChatType.Group => LaanaPeer.Types.Type.Group,
                         _ => LaanaPeer.Types.Type.Buddy
                     },
-                    Uin = record.PeerUin
+                    Uin = PeerUin
                 }
             };
             
-            switch (record.ContentCase)
+            switch (ContentCase)
             {
                 case LaanaMessage.ContentOneofCase.Bubble:
-                    message.Bubble = LaanaMessage.Types.Bubble.Parser.ParseFrom(record.Content);
+                    message.Bubble = LaanaMessage.Types.Bubble.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.File:
-                    message.File = LaanaMessage.Types.NormalFile.Parser.ParseFrom(record.Content);
+                    message.File = LaanaMessage.Types.NormalFile.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.SingleImage:
-                    message.SingleImage = LaanaMessage.Types.SingleImage.Parser.ParseFrom(record.Content);
+                    message.SingleImage = LaanaMessage.Types.SingleImage.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.MarketFace:
-                    message.MarketFace = LaanaMessage.Types.MarketFace.Parser.ParseFrom(record.Content);
+                    message.MarketFace = LaanaMessage.Types.MarketFace.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.Video:
-                    message.Video = LaanaMessage.Types.Video.Parser.ParseFrom(record.Content);
+                    message.Video = LaanaMessage.Types.Video.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.Voice:
-                    message.Voice = LaanaMessage.Types.Voice.Parser.ParseFrom(record.Content);
+                    message.Voice = LaanaMessage.Types.Voice.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.LinkCard:
-                    message.LinkCard = LaanaMessage.Types.LinkCard.Parser.ParseFrom(record.Content);
+                    message.LinkCard = LaanaMessage.Types.LinkCard.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.ContactCard:
-                    message.ContactCard = LaanaMessage.Types.ContactCard.Parser.ParseFrom(record.Content);
+                    message.ContactCard = LaanaMessage.Types.ContactCard.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.MusicCard:
-                    message.MusicCard = LaanaMessage.Types.MusicCard.Parser.ParseFrom(record.Content);
+                    message.MusicCard = LaanaMessage.Types.MusicCard.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.LocationCard:
-                    message.LocationCard = LaanaMessage.Types.LocationCard.Parser.ParseFrom(record.Content);
+                    message.LocationCard = LaanaMessage.Types.LocationCard.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.XmlMessage:
-                    message.XmlMessage = LaanaMessage.Types.XmlMessage.Parser.ParseFrom(record.Content);
+                    message.XmlMessage = LaanaMessage.Types.XmlMessage.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.ArkMessage:
-                    message.ArkMessage = LaanaMessage.Types.ArkMessage.Parser.ParseFrom(record.Content);
+                    message.ArkMessage = LaanaMessage.Types.ArkMessage.Parser.ParseFrom(Content);
                     break;
                 case LaanaMessage.ContentOneofCase.ExtendedMessage:
-                    message.ExtendedMessage = LaanaMessage.Types.ExtendedMessage.Parser.ParseFrom(record.Content);
+                    message.ExtendedMessage = LaanaMessage.Types.ExtendedMessage.Parser.ParseFrom(Content);
                     break;
                 default:
                     throw new Exception("Unexpected message content type.");
